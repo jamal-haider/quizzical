40cec431e57b36c7363e2af6fa62b45363d4aa1a
Jamal <jamal.hyderr@gmail.com>
2023-07-18 17:12:24 +0500

    options changed with pure button and children

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,25 +1,7 @@
 import "./Option.scss"
 
-export default function Option(props){
-
+export default function Option({value, backgroundClass, handleOption}){
     return(
-        <>
-            <input
-              type="radio"
-              id={props.questionId + props.text}
-              name={props.questionId + `-correct-option`}
-              value={props.text}
-              
-               />
-
-            <label
-              onClick={props.handleOption}
-              className={`${props.backgroundClass}`}
-              htmlFor={props.questionId + props.value}
-              
-            >
-              {props.value}
-            </label>
-        </>
+      <button onClick={handleOption} className={backgroundClass}>{value}</button>  
     )
 }
